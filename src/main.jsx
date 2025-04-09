7bd160a2833a7d852289bf8296c8ebccbda66260
Marii R <mariia.revak@gmail.com>
2025-04-09 11:48:58 +0300

    add vercel.json

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router'
+import './index.css';
+import 'modern-normalize';
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
