d281983c5ca981e571cdf86ccd6a0bc67edd926d
Marii R <mariia.revak@gmail.com>
2025-04-09 11:55:35 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router'
 import './index.css';
-import 'modern-normalize';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
